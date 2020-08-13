951e29860c433a4356877a06eb39065520a7db51
Thyago Teles <telestf@users.noreply.github.com>
2020-08-12 22:33:56 -0300

    aula 2

--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -1,15 +1,23 @@
 class Usuario {
-    constructor(id, email, nome) {
-        this.id = id
+    constructor(email, nome) {
+        this.id = this.gerarId()
         this.email = email
         this.nome = nome
+    }
+
+    gerarId() {
+        if (listaUsuarios.length === 0){
+            return 1
+        }
+        return listaUsuarios[listaUsuarios.length-1].id +1
     }
 }
 
 const listaUsuarios = []
 
-listaUsuarios.push(new Usuario(1, "son.goku@dbz.com", "Son Goku"))
-listaUsuarios.push(new Usuario(id=2, email="son.gohan@dbz.com", nome="Son Gohan"))
-listaUsuarios.push(new Usuario(3, "bulma.briefs@dbz.com", "Bulma Briefs"))
+listaUsuarios.push(new Usuario("son.goku@dbz.com", "Son Goku"))
+listaUsuarios.push(new Usuario(email="son.gohan@dbz.com", nome="Son Gohan"))
+listaUsuarios.push(new Usuario("bulma.briefs@dbz.com", "Bulma Briefs"))
 
 module.exports = { Usuario, listaUsuarios }
+
--- backend/routes/api/usuario.js
+++ backend/routes/api/usuario.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { Usuario, listaUsuarios } = require('../../models/usuario')
+const { check, validationResult } = require('express-validator');
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -24,18 +25,18 @@ router.get('/:userId' , (req, res) => {
     }
 })
 
-router.post('/', (req, res) => {
+router.post('/', [
+    check('email', 'email não é válido').isEmail(),
+    check('nome', 'nome não é válido').isLength({min:3}).isAlpha(["pt-BR"])
+], (req, res) => {
     
     try {
         let { email, nome } = req.body
-    
-        if (!email){
-            res.status(400).res.send({"err" : "Coé doido, esqueceu o email?"}) 
-        } else if (!nome) { 
-            res.status(400).res.send({"err" : "Coé doido, esqueceu o nome?"}) 
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors : errors.array()})
         } else {
-            let usuario = new Usuario(id=0, email=email, nome=nome)
-            usuario.id = listaUsuarios[listaUsuarios.length-1].id +1
+            let usuario = new Usuario(email=email, nome=nome)
             listaUsuarios.push(usuario)
             console.log(`${nome} ${email}`)
             res.send(listaUsuarios)
@@ -49,11 +50,17 @@ router.put('/:userId' , (req, res) => {
     try {
         let usuario = listaUsuarios.filter(user => user.id == req.params["userId"])
         if(usuario.length > 0){
-           usuario = usuario[0]
-           let {email, nome} = req.body
-           usuario.email = email
-           usuario.nome = nome
-           res.send(usuario)
+            usuario = usuario[0]
+            let {email, nome} = req.body
+            if (!email){
+                res.status(400).send({"err" : "Coé doido, esqueceu o email?"}) 
+            } else if (!nome) { 
+                res.status(400).send({"err" : "Coé doido, esqueceu o nome?"}) 
+            } else {
+                for (const [chave, valor] of Object.entries(req.body)){
+                    usuario[chave] = valor
+                }
+            }
         } else {
             res.status(404).send({"err" : "Usuário não existe, pooo"})
         }
@@ -66,13 +73,14 @@ router.patch('/:userId' , (req, res) => {
     try {
         let usuario = listaUsuarios.filter(user => user.id == req.params["userId"])
         if(usuario.length > 0){
-            let { email, nome } = req.body
             usuario = usuario[0]
-            if (email) {
-                usuario.email = email
-            }
-            if (nome) {
-                usuario.nome = nome
+            let { email, nome } = req.body
+
+            for (const [chave, valor] of Object.entries(req.body)){
+                if (!valor){
+                    continue
+                }
+                usuario[chave] = valor
             }
             res.send(usuario)
          } else {
